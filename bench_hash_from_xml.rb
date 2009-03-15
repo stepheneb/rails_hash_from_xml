dd88f77826476a7a0c14804666798009b01bf0ab
Stephen Bannasch <stephen.bannasch@gmail.com>
2009-03-15 02:12:54 -0400

    Benchmarking Rails 2.3.1 Hash.from_xml with a 1.7 MB xml file.
    Using XmlMini.backend capability to choose:
    
      REXML (default)
      Nokogiri
      libxml-ruby.
    
    summary: libxml-ruby was 25x faster than REXML
    while nokogiri was only 1.6 times faster

---
+++ bench_hash_from_xml.rb
@@ -0,0 +1,104 @@
+# Benchmark Rails Hash.from_xml conversion on 1.8 MB XML document.
+#
+# file: bench_hash_from_xml.rb
+#
+# To run in Ruby using rails source:
+#
+#   export RAILS_SOURCE=/Users/stephen/dev/ruby/src/gems/rails.git
+#   ruby -I$RAILS_SOURCE/activesupport/lib bench_hash_from_xml.rb
+#
+require 'rubygems'
+require 'benchmark'
+require 'activesupport'
+require 'active_support/xml_mini'
+include ActiveSupport
+
+# This monkeypatch to String adds support for "Nan".to_f => NaN
+Float::NAN_VALUE = *['7FF8000000000000'].pack('H*').unpack('G')
+Float::NAN_STRING = 'NaN'
+class String 
+  alias_method :old_to_f, :to_f 
+  def to_f
+    if self == Float::NAN_STRING
+      Float::NAN_VALUE
+    else
+      old_to_f
+    end
+  end 
+end
+
+if RUBY_PLATFORM =~ /java/
+    require 'jruby'
+    include Java 
+    import javax.xml.parsers.DocumentBuilder
+    import javax.xml.parsers.DocumentBuilderFactory
+    @dbf = DocumentBuilderFactory.new_instance
+    @ruby_info = JRuby.runtime.instance_config.version_string.strip
+    @ruby_info << ", platform: Java, version #{java.lang.System.getProperty('java.version')}"
+  else
+    gem 'nokogiri', '>= 1.1.1'
+    gem 'libxml-ruby', '>= 1.1.2'
+    require 'nokogiri'
+    require 'libxml'
+    @ruby_info = "Ruby version: MRI #{RUBY_VERSION} (#{RUBY_RELEASE_DATE} rev #{RUBY_PATCHLEVEL})"
+    @ruby_info << ", platform: #{RUBY_PLATFORM}"
+end
+
+# require 'ohai'
+# ohai = Ohai::System.new
+# ohai.all_plugins
+# @platform_info = "#{ohai.platform}: #{ohai.platform_version}"
+
+@wg_32937_77360 = File.read("77360.otml")
+
+def hash_from_xml_using_rexml
+  XmlMini.backend = "REXML"
+  Hash.from_xml(@wg_32937_77360)
+end
+
+unless RUBY_PLATFORM =~ /java/ 
+  def hash_from_xml_using_nokogiri
+    XmlMini.backend = "Nokogiri"
+    Hash.from_xml(@wg_32937_77360)
+  end
+
+  def hash_from_xml_using_libxml
+    XmlMini.backend = "LibXML"
+    Hash.from_xml(@wg_32937_77360)
+  end
+end
+
+if RUBY_PLATFORM =~ /java/ 
+  def hash_from_xml_using_jdom_document_builder
+    puts "no implemented ..."
+  end
+end
+
+n = 1
+test_iterations = ARGV.first.to_i 
+test_iterations = 1 unless test_iterations > 1
+puts 
+puts @ruby_info
+puts
+puts "#{n} times: Run Rails Hash.from_xml conversion on 1.8 MB XML document."
+puts
+print "Using Rails ActiveSupport::XmlMini backends: REXML"
+unless RUBY_PLATFORM =~ /java/ 
+  puts ", Nokogiri #{Nokogiri::VERSION}, libxml-ruby 1.1.2"
+else
+  puts
+end
+print "running benchmark "
+if test_iterations == 1 
+  puts "once.\n\n"
+else
+  puts "#{test_iterations} times.\n\n"
+end
+test_iterations.times do
+  Benchmark.bmbm do |x|
+    x.report("rexml") { n.times {hash_from_xml_using_rexml} }
+    x.report("libxml") { n.times {hash_from_xml_using_libxml} }  unless RUBY_PLATFORM =~ /java/ 
+    x.report("nokogiri") { n.times {hash_from_xml_using_nokogiri} }  unless RUBY_PLATFORM =~ /java/ 
+  end
+  puts
+end
---
+++ readme.txt
@@ -0,0 +1,73 @@
+Benchmarking Hash.from_xml with a 1.7 MB xml file in edge Rails 2.3.1 using 
+the new XmlMini.backend capability to choose REXML (default) Nokogiri, or libxml-ruby.
+
+summary: libxml-ruby was 25x faster than REXML while nokogiri was only 1.6 times faster
+
+The results below were collected using this Rails commit:
+
+  Sat Mar 14 10:42:02 2009 -0500
+  http://github.com/rails/rails/commit/7706b57034e91820cf83445aede57c54ab66ac2d
+
+[rails_hash_from_xml]$ export RAILS_SOURCE=/Users/stephen/dev/ruby/src/gems/rails.git
+[rails_hash_from_xml]$ ruby -I$RAILS_SOURCE/activesupport/lib bench_hash_from_xml.rb
+
+Ruby version: MRI 1.8.6 (2008-03-03 rev 114), platform: universal-darwin9.0
+
+1 times: Run Rails Hash.from_xml conversion on 1.8 MB XML document.
+
+Using Rails ActiveSupport::XmlMini backends: REXML, Nokogiri 1.2.1, libxml-ruby 1.1.2
+running benchmark once.
+
+Rehearsal --------------------------------------------
+rexml     12.560000   0.170000  12.730000 ( 13.174433)
+libxml     0.540000   0.010000   0.550000 (  0.572447)
+nokogiri   7.750000   0.090000   7.840000 (  8.041351)
+---------------------------------- total: 21.120000sec
+
+               user     system      total        real
+rexml     12.130000   0.130000  12.260000 ( 12.649212)
+libxml     0.470000   0.010000   0.480000 (  0.493954)
+nokogiri   7.320000   0.080000   7.400000 (  7.781733)
+
+[rails_hash_from_xml]$ jruby -J-server -I$RAILS_SOURCE/activesupport/lib bench_hash_from_xml.rb
+
+jruby 1.3.0 (ruby 1.8.6 patchlevel 287) (2009-03-13 rev 6586) [i386-java], platform: Java, version 1.5.0_16
+
+1 times: Run Rails Hash.from_xml conversion on 1.8 MB XML document.
+
+Using Rails ActiveSupport::XmlMini backends: REXML
+running benchmark once.
+
+Rehearsal -----------------------------------------
+rexml   8.348000   0.000000   8.348000 (  8.348000)
+-------------------------------- total: 8.348000sec
+
+            user     system      total        real
+rexml   4.852000   0.000000   4.852000 (  4.853000)
+
+[rails_hash_from_xml]$ pickjdk
+ 1) 1.3.1
+ 2) 1.4.2
+ 3) 1.5.0 < CURRENT
+ 4) 1.6.0
+ 5) Soylatte-amd64
+ 6) Soylatte16-i386-1.0.3
+ 7) 1.7.0
+ 8) None
+Choose one of the above [1-8]: 5
+
+[rails_hash_from_xml]$ jruby -J-server -I$RAILS_SOURCE/activesupport/lib bench_hash_from_xml.rb
+
+jruby 1.3.0 (ruby 1.8.6 patchlevel 287) (2009-03-13 rev 6586) [amd64-java], platform: Java, version 1.6.0_03-p3
+
+1 times: Run Rails Hash.from_xml conversion on 1.8 MB XML document.
+
+Using Rails ActiveSupport::XmlMini backends: REXML
+running benchmark once.
+
+Rehearsal -----------------------------------------
+rexml   8.585000   0.000000   8.585000 (  8.585000)
+-------------------------------- total: 8.585000sec
+
+            user     system      total        real
+rexml   3.907000   0.000000   3.907000 (  3.907000)
