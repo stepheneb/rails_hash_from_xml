c9f764bca5cfd62560160321535ae8bc65c8028c
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-01-01 21:58:58 -0500

    fix simple jdom test

--- jdom_test.rb
+++ jdom_test.rb
@@ -1,21 +1,20 @@
 require 'jruby'
 include Java 
 
-import javax.xml.parsers.DocumentBuilder
-import javax.xml.parsers.DocumentBuilderFactory
-import java.io.StringReader
-import org.xml.sax.InputSource
+java_import javax.xml.parsers.DocumentBuilder
+java_import javax.xml.parsers.DocumentBuilderFactory
+java_import java.io.StringReader
+java_import org.xml.sax.InputSource
 
 @dbf = DocumentBuilderFactory.new_instance
 
-doc = @dbf.new_document_builder.parse("77360.otml")
+doc = @dbf.new_document_builder.parse("xml/77360.otml")
 puts doc.getElementsByTagName('float').get_length
 
-xml_string = File.read("77360.otml")
+xml_string = File.read("xml/77360.otml")
 xml_string_reader = StringReader.new(xml_string)
 xml_input_source = InputSource.new(xml_string_reader)
 
 doc = @dbf.new_document_builder.parse(xml_input_source)
 
-.get_document_element 
 puts doc.getElementsByTagName('float').get_length
